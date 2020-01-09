2acbc73024d9d94b03179c6a093a1ed69a10f7b1
Danilo <danilofelipe2607@gmail.com>
2020-01-09 14:17:40 -0300

    #84610- correção retorno bancario , adicionado o trim()

--- src/Boleto.Net/Arquivo/DetalheSegmentoURetornoCNAB240.cs
+++ src/Boleto.Net/Arquivo/DetalheSegmentoURetornoCNAB240.cs
@@ -184,7 +184,7 @@ namespace BoletoNet
                 string dataOcorrencia = registro.Substring(137, 8);
                 //DataOcorrencia = Convert.ToDateTime(dataOcorrencia.ToString("##-##-####"));
                 DataOcorrencia = DateTime.ParseExact(dataOcorrencia, "ddMMyyyy", CultureInfo.InvariantCulture);
-                string dataCredito = registro.Substring(145, 8);
+                string dataCredito = registro.Substring(145, 8).Trim();
                 if (!string.IsNullOrEmpty(dataCredito) && (dataCredito) != "00000000")
                 {
                     //DataCredito = Convert.ToDateTime(dataCredito.ToString("##-##-####"));
