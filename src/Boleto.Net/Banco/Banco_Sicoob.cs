245ac7dc8fb847f1dc538c4b50ba945ef2bfb94a
Murillo Jose de Moraes Alves <murilo.alves@tron.com.br>
2020-05-06 08:15:15 -0300

    #89828 - ajuste para cnpj de 14 digitos e nao 15

--- src/Boleto.Net/Banco/Banco_Sicoob.cs
+++ src/Boleto.Net/Banco/Banco_Sicoob.cs
@@ -447,7 +447,7 @@ namespace BoletoNet
                 header += "040";  //Posição 014 a 016     Uso Exclusivo FEBRABAN/CNAB: Brancos
                 header += new string(' ', 1);    //Posição 017           Uso Exclusivo FEBRABAN/CNAB: Brancos
                 header += (cedente.CPFCNPJ.Length == 11 ? "1" : "2");  //Posição 018        1=CPF    2=CGC/CNPJ
-                header += Utils.FormatCode(cedente.CPFCNPJ, "0", 15, true); //Posição 019 a 033   Número de Inscrição da Empresa
+                header += Utils.FormatCode(cedente.CPFCNPJ, "0", 14, true); //Posição 019 a 033   Número de Inscrição da Empresa
                 header += Utils.FormatCode((cedente.Convenio > 0 ? cedente.Convenio.ToString() : ""), " ", 20, true); //Posição 034 a 053     Código do Convênio no Sicoob: Brancos
                 header += Utils.FormatCode(cedente.ContaBancaria.Agencia, "0", 5, true);//Posição 054 a 058     Prefixo da Cooperativa: vide planilha "Capa" deste arquivo
                 header += Utils.FormatCode(cedente.ContaBancaria.DigitoAgencia, "0", 1, true);//Posição 059 a 059
